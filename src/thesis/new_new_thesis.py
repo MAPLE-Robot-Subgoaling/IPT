d3d0d8a9c33b77034b1f0bff5e3e1af5793b223f
Michael Neary <mneary1@umbc.edu>
2017-11-18 22:15:25 -0500

    brought relations out to a separate file

--- src/thesis/new_new_thesis.py
+++ src/thesis/new_new_thesis.py
@@ -1,6 +1,6 @@
 from thesis.visitors import *
 
-from kanren import Relation, facts, run, var, conde, eq
+from kanren import facts, run
 from itertools import combinations
 
 import ast
@@ -9,23 +9,7 @@ import networkx as nx
 import regex as re
 import os
 from thesis.config import *
-
-# declaration of the relations
-has_id = Relation()
-is_before = Relation()
-assigns = Relation()
-uses = Relation()
-hasOutput = Relation()  # line L has output of value V
-
-# define the data dependency relationship
-def depends(a, b):
-    '''there is a dependency between two lines {A, B} if:
-    they share an ID X AND
-    A comes before B AND
-    X is a target for the assignment on line A AND
-    B uses X (not just an assignment where X is a target'''
-    shared_id = var()
-    return conde([is_before(a, b), assigns(a, shared_id), uses(b, shared_id)])
+from thesis.relations import *
 
 def run_thesis(path_to_file, inputs, goals, **kwargs):
 
