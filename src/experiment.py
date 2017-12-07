86ae4966a049d8b7084fb958004f3f6a6c78cfbc
Michael Neary <mneary1@umbc.edu>
2017-12-06 23:31:11 -0500

    inputs and their correct output are now a mapping of input file to goal file instead of a single goal file for every output

--- src/experiment.py
+++ src/experiment.py
@@ -1,19 +1,22 @@
 import os
 from thesis import ExtraneousLineFinder
 
-DATA_SRC  = "/Users/mneary1/Desktop/IPT/data"
+DATA_SRC = "/Users/mneary1/Desktop/IPT/data"
 INPUT_SRC = "/Users/mneary1/Desktop/IPT/tests/inputs"
 GOALS_SRC = "/Users/mneary1/Desktop/IPT/tests/goals"
 OUTPUT_SRC = "/Users/mneary1/Desktop/IPT/tests/outputs/"
 
-hw = "HW3"
-assignment = "hw3_001.py"
-num_inputs = 6
+hw = "HW5"
+assignment = "hw5_001.py"
+num_inputs = len(os.listdir(os.path.join(INPUT_SRC, hw)))
 
 file = os.path.join(DATA_SRC, hw, assignment)
 inputs = [os.path.join(INPUT_SRC, hw, "input{}.txt").format(i) for i in range(1, num_inputs + 1)]
-goals = os.path.join(GOALS_SRC, hw, "goals.txt")
+goals = [os.path.join(GOALS_SRC, hw, "goal{}.txt").format(i) for i in range(1, num_inputs + 1)]
 output_path = os.path.join(OUTPUT_SRC, hw)
+io = dict(zip(inputs, goals))
 
-with ExtraneousLineFinder(file, inputs, goals, output_path, False, True, True) as finder:
-    print("In context")
+with ExtraneousLineFinder(file, io, output_path, False, True, True) as finder:
+    ex = finder.extraneous_lines
+    print(ex)
+
--- src/thesis/ExtraneousLineFinder.py
+++ src/thesis/ExtraneousLineFinder.py
@@ -12,28 +12,33 @@ from thesis.errors import *
 
 class ExtraneousLineFinder:
 
-    def __init__(self, file, inputs, goals,
-                 output='',
+    def __init__(self, file, io, output='',
                  with_execute=False,
                  with_structure=False,
                  with_semantic=False):
-        """:file:   path to the file to analyze
-               :inputs: a list of input file names to be used with :file:
-               :goals:  a file containing the expected output of :file: given :inputs:"""
+        """:file:   path to the assignment that will be analyzed
+           :io: a mapping of input file to correct output files
+           :output: a path to write output to
+           :with_semantic: include semantic dependencies
+           :with_execute: include execution dependencies
+           :with_structure: include structural dependencies"""
 
         # sanity check to make sure that the given files exist before moving on
         assert os.path.isfile(file), "The assignment file {} doesn't exist.".format(file)
-        assert os.path.isfile(goals), "The goals file {} doesn't exist.".format(goals)
 
-        for input_file in inputs:
+        for goal_file in io.values():
+            assert os.path.isfile(goal_file), "The goal file {} doesn't exist.".format(goal_file)
+
+        for input_file in io.keys():
             assert os.path.isfile(input_file), "The input file {} doesn't exist.".format(input_file)
 
         self.file = file
-        self.inputs = inputs
+        self.io = io
 
-        # TODO: turn single goals file into multiple files, one for each input. Read in the goals according to the current input being tested
-        with open(goals) as g:
-            self.goals = [line.strip() for line in g.readlines()]
+        # TODO: turn single goals file into multiple files, one for each input. Read in the goals
+        # according to the current input being tested
+        #with open(goals) as g:
+        #    self.goals = [line.strip() for line in g.readlines()]
 
         self.extraneous = {}
         self.extraneous_lines = []
@@ -100,10 +105,10 @@ class ExtraneousLineFinder:
         file_name = os.path.splitext(os.path.basename(self.file))[0]
         output_path = os.path.join(self.output, file_name + ".out")
         cmd = "python3 -m trace --trace {} < {} > {}".format(self.file, input_name, output_path)
-        cmdresult = os.system(cmd)
+        cmd_result = os.system(cmd)
 
         # TODO: maybe I don't want to raise an exception here
-        if cmdresult != 0:
+        if cmd_result != 0:
             raise RuntimeError("Failed to execute a trace of <" + self.file + ">")
 
         # determine the line number of each line that was executed
@@ -137,15 +142,20 @@ class ExtraneousLineFinder:
         return executed_lines
 
     def __enter__(self):
-        print("Entering the context...")
         print("File being tested: {}".format(self.file))
         results_so_far = self._bootstrap_facts()
 
         # find the output and determine its correctness for each input file
-        for input_name in self.inputs:
+        for input_file, goal_file in self.io.items():
+
+            print("Iteration <{}> <{}>".format(input_file, goal_file))
+
+            # read in the corresponding goal file
+            with open(goal_file) as f:
+                goals = f.readlines()
 
             # determine the execution dependency pairs
-            executed_lines = self._run_trace(input_name)
+            executed_lines = self._run_trace(input_file)
             execution_dependencies = []
             for i in range(len(executed_lines) - 1):
                 a = executed_lines[i]
@@ -159,24 +169,22 @@ class ExtraneousLineFinder:
             # get the lines that give correct output
             correct_lines = []
             for line, output in result_outputs:
-                for goal in self.goals:
+                for goal in goals:
                     found_goal = re.findall(re.escape(goal), output, overlapped=True)
                     if line in executed_lines and len(found_goal) == 1:
                         correct_lines.append((line, output))
 
             # if no matches were found, terminate execution
-            #TODO: might want to rethink throwing an exception here...
+            # TODO: might want to rethink throwing an exception here...
             if len(correct_lines) == 0:
-                raise GoalNotFoundError("Correct output not found with input {}".format(input_name))
+                raise GoalNotFoundError("Correct output not found with input {}".format(input_file))
 
             # add edges to the directed dependency graph from the semantic and structural depedencies found so far
             # edges between line pairs flipped so it can look from the goal line(s) to the beginning
             # adding an edge that already exists with not add a double edge to the graph
             self.ddg.add_edges_from([(b, a) for a, b in results_so_far])
-            #TODO: make sure that the graph nodes are actually working
+            # TODO: make sure that the graph nodes are actually working
 
-            print("Iteration <{}>".format(input_name))
-            print("\t edges: {}".format(self.ddg.edges()))
 
             # add execution dependencies to the results
             # again, ading edges "backwards" so we can search from the goal forward
@@ -204,11 +212,9 @@ class ExtraneousLineFinder:
 
             extraneous_lines = list(unused_nodes)
 
-            # thinks that empty lines are extraneous
+            # thinks that empty lines, and function defs are extraneous
             for i in range(len(unused_nodes)):
-
                 node = unused_nodes[i]
-
                 line = self.src_lines[node - 1]
                 if line.isspace() or len(line) == 0:
                     extraneous_lines.remove(node)
@@ -232,12 +238,11 @@ class ExtraneousLineFinder:
 
         # An extraneous line is one that appears as extraneous for all given inputs
         for key, val in self.extraneous.items():
-            if val == len(self.inputs):
+            if val == len(self.io):
                 self.extraneous_lines.append(key)
 
-        #print("Extraneous lines: {}".format(self.extraneous_lines))
+        # print("Extraneous lines: {}".format(self.extraneous_lines))
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         reset_all_relations()
-        print("Exiting the context...")
